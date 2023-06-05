0c44d8f4b1012244a52ca0909197f8206498b7bc
dreamjbj1 <999888@naver.com>
2023-06-05 18:23:39 +0900

    constructor

--- Array234/src/inheritance/CustomerTest2.java
+++ Array234/src/inheritance/CustomerTest2.java
@@ -3,7 +3,7 @@ package inheritance;
 public class CustomerTest2 {
 
 	public static void main(String[] args) {
-		VIPCustomer customerKim = new VIPCustomer();
+		VIPCustomer customerKim = new VIPCustomer(10020, "김유신", 11);
 		customerKim.setCustomerID(10020);
 		customerKim.setCustomerName("김유신");
 		customerKim.bonusPoint = 10000;
